a329a96b0ef60f8da0f5a2f6a85c5dded09b9f4c
wobbuuu <wobbuuu@users.noreply.github.com>
2020-07-20 15:33:31 +0300

    add 'both' to get_noise

--- noisypy/noisy_utils.py
+++ noisypy/noisy_utils.py
@@ -357,7 +357,7 @@ def get_noise(indexes, calib, G, name='./data/cond', iters=20, skip_indexes=[],\
               reg_grid=None, max_deviation={'v':15e-6, 'i':4e-10, 'det':2.5e-3},\
               savgolize={'v':{'window_length':3,'polyorder':1,},}, savgol_v=[], savgol_i=[],\
               savgol_r=[3, 1], rpar=varz.r0/2, skip_points=(0, 0), T=4.2, fitrange=None,\
-              aux_rdif=None, rkwargs={}, verbose=True, fit_offset=False):
+              aux_rdif=None, rkwargs={}, verbose=True, fit_offset=False, both=False):
     """
     savgolize only dict of dicts
     
@@ -428,9 +428,14 @@ def get_noise(indexes, calib, G, name='./data/cond', iters=20, skip_indexes=[],\
             current, voltage = get_r(df, **rkwargs)
             if i+j in skip_indexes:
                 continue
-            cur = pd.concat([cur, current], axis=1, ignore_index=True)
-            v = pd.concat([v, voltage], axis=1, ignore_index=True)
-            det = pd.concat([det, df[det_ind]], axis=1, ignore_index=True)   
+	    if both and j%2 == 1:
+	        cur = pd.concat([cur, current[::-1]], axis=1, ignore_index=True)
+	        v = pd.concat([v, voltage[::-1]], axis=1, ignore_index=True)
+	        det = pd.concat([det, df[det_ind][::-1]], axis=1, ignore_index=True)   
+	    else:
+	        cur = pd.concat([cur, current], axis=1, ignore_index=True)
+	        v = pd.concat([v, voltage], axis=1, ignore_index=True)
+	        det = pd.concat([det, df[det_ind]], axis=1, ignore_index=True)   
         last_df = df
         
         good_inds = (v.abs() < threshold['v']) & (cur.abs() < threshold['i']) &\
